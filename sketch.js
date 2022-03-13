97882c92d5b01312d104a7a362c87f498f56c6d1
Jack Woodrow <jackwoodrow99@gmail.com>
2022-03-12 23:55:54 -0500

    Create randomized 2d arr to represent game boeard and draw it to screen

--- sketch.js
+++ sketch.js
@@ -1,13 +1,34 @@
+var board = [];
 var canvas = null;
 
 function setup() {
 	canvas = createCanvas(500, 500);
 	canvas.parent('p5-canvas');
 
+	board = randomizeArray(50, 50, 0.01);
+
 	background(200);
 }
 
 function draw() {
-	noStroke();
-	square(50, 50, 50);
+	board.forEach((row, x) => {
+		row.forEach((box, y) => {
+			box ? square(x * 10, y * 10, 10) : false;
+		});
+	});
+}
+
+function randomizeArray(x, y, randomFactor) {
+	let board = [];
+	for (let i = 0; i < x; i++) {
+		board.push([]);
+		for (let j = 0; j < y; j++) {
+			if (Math.random() <= randomFactor) {
+				board[i].push(1);
+			} else {
+				board[i].push(0);
+			}
+		}
+	}
+	return board;
 }
